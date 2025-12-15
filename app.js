aab810788c7ab5220064f34689d4578d310cc9fa
runyakol-png <runya.kol@gmail.com>
2025-12-15 13:04:16 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-function openSection(section) {
-  const payload = {
-    type: "open_section",
-    section: section,
-    time: Date.now()
-  };
-
-  if (tg) {
-    tg.sendData(JSON.stringify(payload));
-    tg.showAlert("Открыт раздел: " + section);
-  } else {
-    console.log("OPEN:", section);
-  }
+function login() {
+  document.getElementById('login-screen').classList.remove('active');
+  document.getElementById('dashboard-screen').classList.add('active');
 }
